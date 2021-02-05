bdb3904ddc67430ba64abfe9b46035c007d1e043
Laurent Leleux <laurent.leleux@vinci.be>
2021-02-05 15:34:13 +0100

    Initial commit

---
+++ src/main/java/be/vinci/pae/main/Main.java
@@ -0,0 +1,18 @@
+package be.vinci.pae.main;
+
+/**
+ * Main class.
+ *
+ */
+public class Main {
+
+  /**
+   * Main method.
+   * 
+   * @param args command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+}
---
+++ src/test/java/be/vinci/pae/DemoTest.java
@@ -0,0 +1,14 @@
+package be.vinci.pae;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.Test;
+
+public class DemoTest {
+
+  @Test
+  public void demoTest() {
+    assertTrue(true);
+  }
+
+
+}
