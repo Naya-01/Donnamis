d4408cdaffee346b8f826f6cb0e1c10f7450ce3f
Laurent Leleux <laurent.leleux@vinci.be>
2022-02-11 11:23:37 +0100

    fix: checkstyles

--- src/main/java/be/vinci/pae/main/Main.java
+++ src/main/java/be/vinci/pae/main/Main.java
@@ -2,13 +2,12 @@ package be.vinci.pae.main;
 
 /**
  * Main class.
- *
  */
 public class Main {
 
   /**
    * Main method.
-   * 
+   *
    * @param args command line arguments
    */
   public static void main(String[] args) {
