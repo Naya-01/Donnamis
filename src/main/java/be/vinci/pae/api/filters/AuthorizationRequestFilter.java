0f906dac66b038f306934c326dafeedbfc1be239
Stefan Mircovici <stefan.mircovici@student.vinci.be>
2022-02-21 12:37:58 +0100

    fix content of file AuthorizationRequestFilter.java because of unused private fields

--- src/main/java/be/vinci/pae/api/filters/AuthorizationRequestFilter.java
+++ src/main/java/be/vinci/pae/api/filters/AuthorizationRequestFilter.java
@@ -1,9 +1,5 @@
 package be.vinci.pae.api.filters;
 
-import be.vinci.pae.utils.Config;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.JWTVerifier;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.container.ContainerRequestContext;
@@ -17,9 +13,8 @@ import java.io.IOException;
 @Authorize
 public class AuthorizationRequestFilter implements ContainerRequestFilter {
 
-  private final Algorithm jwtAlgorithm = Algorithm.HMAC256(Config.getProperty("JWTSecret"));
-  private final JWTVerifier jwtVerifier = JWT.require(this.jwtAlgorithm).withIssuer("auth0")
-      .build();
+  //private final Algorithm jwtAlgorithm = Algorithm.HMAC256(Config.getProperty("JWTSecret"));
+  //private final JWTVerifier jwtVerifier = JWT.require(this.jwtAlgorithm).withIssuer("auth0").build();
 
   @Inject
   //private UserDataService myUserDataService;
