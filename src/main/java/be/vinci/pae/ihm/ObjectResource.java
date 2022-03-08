320f301f28f594444df45addc817ff6c6222e439
Rayan Abarkan <rayan.abarkan@student.vinci.be>
2022-03-08 16:44:09 +0100

    [ADD] implement object resource with first route 'getObject' by his id

---
+++ src/main/java/be/vinci/pae/ihm/ObjectResource.java
@@ -0,0 +1,35 @@
+package be.vinci.pae.ihm;
+
+import be.vinci.pae.business.domain.dto.ObjectDTO;
+import be.vinci.pae.business.ucc.ObjectUCC;
+import be.vinci.pae.ihm.filters.Authorize;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Singleton
+@Path("/object")
+public class ObjectResource {
+
+  private static final ObjectMapper jsonMapper = new ObjectMapper();
+
+  @Inject
+  private ObjectUCC objectUCC;
+
+  @GET
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Authorize
+  public ObjectNode getObject(@PathParam("id") int id) {
+    ObjectDTO objectDTO = objectUCC.getObject(id);
+
+    return jsonMapper.createObjectNode()
+        .putPOJO("object", objectDTO);
+  }
+}
