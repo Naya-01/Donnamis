8007e5a2ca267636d6f105bc1b02939a1d7dee48
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-03-10 19:14:55 +0100

    [ADD] add Address DTO and AddressImpl

---
+++ src/main/java/be/vinci/pae/business/domain/AddressImpl.java
@@ -0,0 +1,85 @@
+package be.vinci.pae.business.domain;
+
+import be.vinci.pae.business.domain.dto.AddressDTO;
+
+public class AddressImpl implements AddressDTO {
+
+  private int idAddress;
+  private String unitNumber;
+  private String buildingNumber;
+  private String street;
+  private String postcode;
+  private String commune;
+  private String country;
+
+
+  @Override
+  public int getIdAddress() {
+    return this.idAddress;
+  }
+
+  @Override
+  public void setIdAddress(int idAddress) {
+    this.idAddress = idAddress;
+  }
+
+  @Override
+  public String getUnitNumber() {
+    return this.unitNumber;
+  }
+
+  @Override
+  public void setUnitNumber(String unitNumber) {
+    this.unitNumber = unitNumber;
+  }
+
+  @Override
+  public String getBuildingNumber() {
+    return this.buildingNumber;
+  }
+
+  @Override
+  public void setBuildingNumber(String buildingNumber) {
+    this.buildingNumber = buildingNumber;
+  }
+
+  @Override
+  public String getStreet() {
+    return this.street;
+  }
+
+  @Override
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  @Override
+  public String getPostCode() {
+    return this.postcode;
+  }
+
+  @Override
+  public void setPostcode(String postcode) {
+    this.postcode = postcode;
+  }
+
+  @Override
+  public String getCommune() {
+    return this.commune;
+  }
+
+  @Override
+  public void setCommune(String commune) {
+    this.commune = commune;
+  }
+
+  @Override
+  public String getCountry() {
+    return this.country;
+  }
+
+  @Override
+  public void setCountry(String country) {
+    this.country = country;
+  }
+}
---
+++ src/main/java/be/vinci/pae/business/domain/dto/AddressDTO.java
@@ -0,0 +1,36 @@
+package be.vinci.pae.business.domain.dto;
+
+import be.vinci.pae.business.domain.AddressImpl;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(as = AddressImpl.class)
+public interface AddressDTO {
+
+  int getIdAddress();
+
+  void setIdAddress(int idAddress);
+
+  String getUnitNumber();
+
+  void setUnitNumber(String unitNumber);
+
+  String getBuildingNumber();
+
+  void setBuildingNumber(String buildingNumber);
+
+  String getStreet();
+
+  void setStreet(String street);
+
+  String getPostCode();
+
+  void setPostcode(String postcode);
+
+  String getCommune();
+
+  void setCommune(String commune);
+
+  String getCountry();
+
+  void setCountry(String country);
+}
