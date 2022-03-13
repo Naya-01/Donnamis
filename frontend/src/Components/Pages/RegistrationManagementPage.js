65f70aa486c04bcba44579d664e9d2a15de6f2f1
Mehdi Bouchbouk <mehdi.bouchbouk@student.vinci.be>
2022-03-13 20:42:53 +0100

    [ADD] Filter

--- frontend/src/Components/Pages/RegistrationManagementPage.js
+++ frontend/src/Components/Pages/RegistrationManagementPage.js
@@ -7,6 +7,16 @@ const htmlPage = `
                         
                         <!-- Search Bar -->
                         <div class="input-group mb-3 mt-5" >
+                        <div class="btn-group mx-2" role="group" aria-label="Basic radio toggle button group">
+                          <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                          <label class="btn btn-outline-secondary" for="btnradio3">Aucun</label>
+                          
+                          <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off">
+                          <label class="btn btn-outline-dark" for="btnradio1">En attente</label>
+                        
+                          <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+                          <label class="btn btn-outline-danger" for="btnradio2">Refusé</label>
+                        </div>
                           <input type="text" class="form-control fs-4" id="searchBar" placeholder="Rechercher une demande d'inscription">
                           <button class="btn btn-outline-primary fs-4" id="searchButton" type="button">Rechercher</button>
                         </div>
