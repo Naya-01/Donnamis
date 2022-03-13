5bf56375d0414e78f4cae91929d80829ca887da2
François Bardijn <francois.bardijn@student.vinci.be>
2022-03-14 00:22:45 +0100

    [ADD] route to get last 6 offers

--- src/main/java/be/vinci/pae/business/ucc/OfferUCC.java
+++ src/main/java/be/vinci/pae/business/ucc/OfferUCC.java
@@ -4,5 +4,20 @@ import be.vinci.pae.business.domain.dto.OfferDTO;
 import java.util.List;
 
 public interface OfferUCC {
+
+  /**
+   * Get all the offers that matche with a search pattern.
+   *
+   * @param searchPattern the search pattern to find offers according to their type, description
+   * @return a list of all offerDTO that match with the search pattern
+   */
   List<OfferDTO> getAllPosts(String searchPattern);
+
+  /**
+   * Get the last six offers posted.
+   *
+   * @return a list of six offerDTO
+   */
+  List<OfferDTO> getLastOffers();
+
 }
