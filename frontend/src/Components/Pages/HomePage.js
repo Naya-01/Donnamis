ea9fbe55c5157635bebd41bd22bd8478bb674488
Stefan <stefan.mircovici@student.vinci.be>
2022-03-20 13:19:52 +0100

    [REF] refactor the loop that render the home page depending of number of offers

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -6,22 +6,46 @@ const offerLibrary = new OfferLibrary();
  * Render the HomePage
  */
 const render = async () => {
-  //let lastOffers = await offerLibrary.getAllLastOffers();
+  let lastOffers = await offerLibrary.getAllLastOffers();
+  let lines;
+  let nbOffers = lastOffers.length;
+  if (lastOffers.length <= 3) {
+    lines = 1;
+  } else {
+    lines = 2;
+  }
+
   let page = "";
-  for (let i = 0; i < 2; i++) {
+  for (let nbRows = 0; nbRows < lines; nbRows++) {
     page += `<div class="container-fluid align-content-center w-75 mt-3 mb-3">
       <div class="row row-cols-1 row-cols-md-3 g-4">`;
-    for (let j = 0; j < 3; j++) {
-      page += `<div class="col">
+
+    //number max of columns =3
+    let nbColumns = 0;
+    //check if we still have offers and if the arrived to the end of row
+    while (nbColumns < 3 && nbOffers !== 0) {
+      page += `
+        <div class="col">
           <div class="card">
-      <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+            <svg class="bd-placeholder-img card-img-top" width="100%" 
+              height="180" xmlns="http://www.w3.org/2000/svg" role="img" 
+              aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid 
+              slice" focusable="false"><title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#868e96"></rect><text 
+              x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+            </svg>
     
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text">This is a longer card with supporting text 
+                below as a natural lead-in to additional content. This content 
+                is a little bit longer.
+               </p>
             </div>
           </div>
         </div>`;
+      nbOffers--;
+      nbColumns++;
     }
     page += `</div>
     </div>`;
