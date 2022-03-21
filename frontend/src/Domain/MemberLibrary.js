d214f613bd563a83596bf684ce9c689f0ec9c2db
Mehdi <mehdi.bouchbouk@student.vinci.be>
2022-03-21 09:33:15 +0100

    [REF] Auth ressource get user from id moved to Member ressource

--- frontend/src/Domain/MemberLibrary.js
+++ frontend/src/Domain/MemberLibrary.js
@@ -14,7 +14,7 @@ class MemberLibrary {
           "Authorization": getSessionObject("user").accessToken,
         },
       };
-      response = await fetch("api/auth/getuserbytoken", options);
+      response = await fetch("/api/member/getMemberByToken/", options);
     } catch (err) {
       console.log(err);
     }
@@ -35,7 +35,7 @@ class MemberLibrary {
           "Authorization": getSessionObject("user").accessToken,
         },
       };
-      response = await fetch("api/auth/id/" + id, options);
+      response = await fetch("api/member/id/" + id, options);
     } catch (err) {
       console.log(err);
     }
