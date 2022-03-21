707e2ac7fbfebe7bae0693a4f2127a2c32a60978
rayan abarkan <rayan.abarkan@student.vinci.be>
2022-03-21 19:57:40 +0100

    [ADD] implement the Logger & Adapted the exception mapper

---
+++ src/main/java/be/vinci/pae/utils/Log.java
@@ -0,0 +1,29 @@
+package be.vinci.pae.utils;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class Log {
+
+  private static Logger logger;
+
+  public static void config() {
+    logger = Logger.getLogger("Log");
+    logger.setLevel(Level.INFO);
+    try {
+      FileHandler fileHandler = new FileHandler("LogFile%g.log", 50000, 5, true);
+      fileHandler.setLevel(Level.ALL);
+      logger.addHandler(fileHandler);
+      SimpleFormatter formatter = new SimpleFormatter();
+      fileHandler.setFormatter(formatter);
+      logger.info("Init Logger");
+      System.out.println(Logger.getLogger("Log"));
+      System.out.println(logger);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
--- src/main/java/be/vinci/pae/utils/WebExceptionMapper.java
+++ src/main/java/be/vinci/pae/utils/WebExceptionMapper.java
@@ -2,18 +2,29 @@ package be.vinci.pae.utils;
 
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.StatusType;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Provider
 public class WebExceptionMapper implements ExceptionMapper<Throwable> {
+
   @Override
   public Response toResponse(Throwable exception) {
-    exception.printStackTrace();
-    if (exception instanceof WebApplicationException)
-      return ((WebApplicationException) exception).getResponse(); // the response is already prepared
-    return  Response.status( Response.Status.INTERNAL_SERVER_ERROR)
+//    exception.printStackTrace();
+    System.out.println(Logger.getLogger("Log"));
+    Logger.getLogger("Log").log(Level.INFO, exception.getMessage());
+    if (exception instanceof WebApplicationException) {
+      return Response.status( ((WebApplicationException) exception).getResponse().getStatus())
+          .entity(exception.getMessage())
+          .build();
+    }
+
+    return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
         .entity(exception.getMessage())
         .build();
   }
+
 }
