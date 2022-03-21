fd6941c5dda40ebec04b037918f59bfaf9fdd50d
rayan abarkan <rayan.abarkan@student.vinci.be>
2022-03-21 19:47:57 +0100

    [REF] rename exception internal server error to FatalException

--- src/main/java/be/vinci/pae/business/exceptions/FatalException.java
+++ src/main/java/be/vinci/pae/business/exceptions/FatalException.java
@@ -3,9 +3,9 @@ package be.vinci.pae.business.exceptions;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response;
 
-public class InternalServerErrorException extends WebApplicationException {
+public class FatalException extends WebApplicationException {
 
-  public InternalServerErrorException() {
+  public FatalException() {
     super(Response.Status.INTERNAL_SERVER_ERROR);
   }
 
@@ -14,7 +14,7 @@ public class InternalServerErrorException extends WebApplicationException {
    *
    * @param message custom error message
    */
-  public InternalServerErrorException(String message) {
+  public FatalException(String message) {
     super(message, Response.Status.INTERNAL_SERVER_ERROR);
   }
 }
