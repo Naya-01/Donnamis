fe10130010f81b3603dbf958b7220b8e381383c7
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-03-25 11:44:14 +0100

    [ADD] add the testGetTypeWithGoodId test to test the TypeUCC

---
+++ src/test/java/be/vinci/pae/business/ucc/TypeUCCImplTest.java
@@ -0,0 +1,46 @@
+package be.vinci.pae.business.ucc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import be.vinci.pae.TestBinder;
+import be.vinci.pae.business.domain.dto.TypeDTO;
+import be.vinci.pae.dal.dao.TypeDAO;
+import be.vinci.pae.dal.services.DALService;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class TypeUCCImplTest {
+
+  private TypeUCC typeUCC;
+  private TypeDAO mockTypeDAO;
+  private TypeDTO mockRealType;
+  private DALService mockDalService;
+
+  @BeforeEach
+  void initAll() {
+    ServiceLocator locator = ServiceLocatorUtilities.bind(new TestBinder());
+    this.typeUCC = locator.getService(TypeUCC.class);
+    this.mockTypeDAO = locator.getService(TypeDAO.class);
+    this.mockRealType = locator.getService(TypeDTO.class);
+    this.mockDalService = locator.getService(DALService.class);
+    Mockito.when(mockRealType.getIdType()).thenReturn(1);
+    Mockito.when(mockRealType.getTypeName()).thenReturn("Bon type");
+  }
+
+  @DisplayName("Test getType with id function with an id that correspond to an existing type")
+  @Test
+  public void testGetTypeWithGoodId(){
+    Mockito.when(mockTypeDAO.getOne(mockRealType.getIdType())).thenReturn(mockRealType);
+    assertAll(
+        () -> assertEquals(typeUCC.getType(mockRealType.getIdType()), mockRealType),
+        () -> Mockito.verify(mockDalService).startTransaction(),
+        () -> Mockito.verify(mockDalService).commitTransaction(),
+        () -> Mockito.verify(mockDalService,Mockito.never()).rollBackTransaction()
+    );
+
+  }
+}
