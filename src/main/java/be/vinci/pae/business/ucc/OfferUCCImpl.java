380017ce1e48465ce5c63ff306e2ee8be2068b8a
Stefan <stefan.mircovici@student.vinci.be>
2022-03-26 18:34:44 +0100

    [ADD] test get all last offers with an empty list of offers received from dao

--- src/main/java/be/vinci/pae/business/ucc/OfferUCCImpl.java
+++ src/main/java/be/vinci/pae/business/ucc/OfferUCCImpl.java
@@ -35,7 +35,6 @@ public class OfferUCCImpl implements OfferUCC {
       dalService.startTransaction();
       offers = offerDAO.getAllLast();
       if (offers.isEmpty()) {
-        dalService.rollBackTransaction();
         throw new NotFoundException("Aucune offres");
       }
     } catch (Exception e) {
--- src/test/java/be/vinci/pae/business/ucc/OfferUCCImplTest.java
+++ src/test/java/be/vinci/pae/business/ucc/OfferUCCImplTest.java
@@ -1,10 +1,42 @@
 package be.vinci.pae.business.ucc;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import be.vinci.pae.TestBinder;
+import be.vinci.pae.dal.dao.OfferDAO;
+import be.vinci.pae.dal.services.DALService;
+import be.vinci.pae.exceptions.NotFoundException;
+import java.util.ArrayList;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class OfferUCCImplTest {
 
+  private DALService mockDalService;
+  private OfferDAO offerDAO;
+  private OfferUCC offerUCC;
+
   @BeforeEach
   void setUp() {
+    ServiceLocator locator = ServiceLocatorUtilities.bind(new TestBinder());
+    this.mockDalService = locator.getService(DALService.class);
+    this.offerDAO = locator.getService(OfferDAO.class);
+    this.offerUCC = locator.getService(OfferUCC.class);
+  }
+
+  @DisplayName("Test get last offers avec aucune offre recue du DAO")
+  @Test
+  public void testGetAllLastOffersWithDAOReturningEmptyListOfOffers() {
+    Mockito.when(offerDAO.getAllLast()).thenReturn(new ArrayList<>());
+    assertAll(
+        () -> assertThrows(NotFoundException.class, () -> offerUCC.getLastOffers()),
+        () -> Mockito.verify(mockDalService, Mockito.atLeastOnce()).startTransaction(),
+        () -> Mockito.verify(mockDalService, Mockito.atLeastOnce()).rollBackTransaction()
+    );
   }
 }
