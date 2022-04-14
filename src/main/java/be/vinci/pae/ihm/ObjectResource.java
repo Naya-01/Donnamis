023f1241d06f94f8743dcf74d46aa1b78b2f4304
Rayan Abarkan <rayan.abarkan@student.vinci.be>
2022-04-14 12:50:08 +0200

    [REM] remove function to mark to not collected

--- src/main/java/be/vinci/pae/ihm/ObjectResource.java
+++ src/main/java/be/vinci/pae/ihm/ObjectResource.java
@@ -4,7 +4,6 @@ import be.vinci.pae.business.domain.dto.MemberDTO;
 import be.vinci.pae.business.domain.dto.ObjectDTO;
 import be.vinci.pae.business.ucc.ObjectUCC;
 import be.vinci.pae.exceptions.BadRequestException;
-import be.vinci.pae.exceptions.ForbiddenException;
 import be.vinci.pae.exceptions.NotFoundException;
 import be.vinci.pae.exceptions.UnauthorizedException;
 import be.vinci.pae.ihm.filters.Authorize;
@@ -135,33 +134,5 @@ public class ObjectResource {
     return objectUCC.updateOne(objectDTO);
   }
 
-
-  /**
-   * Mark an object to 'not collected'.
-   *
-   * @param objectDTO object with his id
-   * @return an object
-   */
-  @POST
-  @Path("/notCollected")
-  @Produces(MediaType.APPLICATION_JSON)
-  @Consumes(MediaType.APPLICATION_JSON)
-  @Authorize
-  public ObjectDTO notCollectedObject(@Context ContainerRequest request, ObjectDTO objectDTO) {
-
-    if (objectDTO.getIdObject() == null) {
-      throw new BadRequestException("Veuillez indiquer un id dans l'objet de la ressource ");
-    }
-
-    objectDTO = objectUCC.getObject(objectDTO.getIdObject());
-
-    MemberDTO ownerDTO = (MemberDTO) request.getProperty("user");
-    if (!ownerDTO.getMemberId().equals(objectDTO.getIdOfferor())) {
-      throw new ForbiddenException("Cet objet ne vous appartient pas");
-    }
-
-    return objectUCC.notCollectedObject(objectDTO);
-
-  }
 
 }
