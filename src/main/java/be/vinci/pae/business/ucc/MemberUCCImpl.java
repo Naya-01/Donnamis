f8b918539b0313e98f5f00b1ec201945bd9a7046
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-04-16 15:43:31 +0200

    [REF] refactor updateMember to not always update the adress

--- src/main/java/be/vinci/pae/business/ucc/MemberUCCImpl.java
+++ src/main/java/be/vinci/pae/business/ucc/MemberUCCImpl.java
@@ -243,13 +243,16 @@ public class MemberUCCImpl implements MemberUCC {
   public MemberDTO updateMember(MemberDTO memberDTO) {
     try {
       dalService.startTransaction();
-      AddressDTO addressDTO = null;
+      AddressDTO addressDTO ;
       if(memberDTO.getAddress() != null){
         memberDTO.getAddress().setIdMember(memberDTO.getMemberId());
         addressDTO = addressDAO.updateOne(memberDTO.getAddress());
         if (addressDTO == null) {
           throw new ForbiddenException("Problem with updating address");
         }
+      }
+      else{
+        addressDTO = addressDAO.getAddressByMemberId(memberDTO.getMemberId());
       }
       MemberDTO modifierMemberDTO = memberDAO.updateOne(memberDTO);
       if (modifierMemberDTO == null) {
