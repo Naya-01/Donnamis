6cf8382b5c46620b1f90eafac82afe48b466692e
rayan abarkan <rayan.abarkan@student.vinci.be>
2022-04-19 19:28:53 +0200

    [ADD] add logger in the different AddresseResource

--- src/main/java/be/vinci/pae/utils/Log.java
+++ src/main/java/be/vinci/pae/utils/Log.java
@@ -2,7 +2,6 @@ package be.vinci.pae.utils;
 
 import java.io.IOException;
 import java.util.logging.FileHandler;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
