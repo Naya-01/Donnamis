3d8b49e9b65302b91c8551db54a26b6882a9e2f9
Rayan Abarkan <rayan.abarkan@student.vinci.be>
2022-04-23 17:59:40 +0200

    [FIX] warning checkstyle

--- src/main/java/be/vinci/pae/business/ucc/ObjectUCC.java
+++ src/main/java/be/vinci/pae/business/ucc/ObjectUCC.java
@@ -1,7 +1,6 @@
 package be.vinci.pae.business.ucc;
 
 import be.vinci.pae.business.domain.dto.ObjectDTO;
-import be.vinci.pae.business.domain.dto.OfferDTO;
 import java.awt.image.BufferedImage;
 import java.util.List;
 
