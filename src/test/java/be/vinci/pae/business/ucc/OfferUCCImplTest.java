d0edab8f8eaf071e9ae6d87152f8f78a41829634
Stefan <stefan.mircovici@student.vinci.be>
2022-04-27 20:52:13 +0200

    [ADD] test giveOffer with not same version object in db and front

--- src/test/java/be/vinci/pae/business/ucc/OfferUCCImplTest.java
+++ src/test/java/be/vinci/pae/business/ucc/OfferUCCImplTest.java
@@ -510,6 +510,34 @@ class OfferUCCImplTest {
     );
   }
 
+  @DisplayName("Test giveOffer with not same version object in db and front")
+  @Test
+  public void testGiveOfferWithNotSameVersionObjectDbAndFrom() {
+    OfferDTO offerDTO = getNewOffer();
+    offerDTO.setIdOffer(3);
+    offerDTO.getObject().setIdObject(3);
+    offerDTO.getObject().setIdOfferor(2);
+    offerDTO.getObject().setVersion(2);
+    offerDTO.setVersion(2);
+    OfferDTO offerDTOFromDao = getNewOffer();
+    offerDTOFromDao.setIdOffer(3);
+    offerDTOFromDao.getObject().setIdObject(3);
+    offerDTOFromDao.getObject().setIdOfferor(2);
+    offerDTOFromDao.setVersion(2);
+    offerDTOFromDao.getObject().setVersion(5);
+    MemberDTO memberDTO = memberFactory.getMemberDTO();
+    memberDTO.setMemberId(2);
+
+    Mockito.when(offerDAO.getLastObjectOffer(offerDTO.getObject().getIdObject()))
+        .thenReturn(offerDTOFromDao);
+
+    assertAll(
+        () -> assertThrows(ForbiddenException.class, () -> offerUCC.giveOffer(offerDTO, memberDTO)),
+        () -> Mockito.verify(mockDalService, Mockito.atLeastOnce()).startTransaction(),
+        () -> Mockito.verify(mockDalService, Mockito.atLeastOnce()).rollBackTransaction()
+    );
+  }
+
   @DisplayName("Test giveOffer without having interest")
   @Test
   public void testGiveOfferWithoutHavingAnyInterest() {
