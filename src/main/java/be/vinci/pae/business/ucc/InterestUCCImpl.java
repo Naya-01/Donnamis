050a6661eaa06f2fb03058e5f2bdf4248d9c28ef
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-04-30 15:32:08 +0200

    [REF] delete useless conditions

--- src/main/java/be/vinci/pae/business/ucc/InterestUCCImpl.java
+++ src/main/java/be/vinci/pae/business/ucc/InterestUCCImpl.java
@@ -153,10 +153,8 @@ public class InterestUCCImpl implements InterestUCC {
         throw new ForbiddenException("Les versions ne correspondent pas");
       }
 
-      if ((!offerDTO.getStatus().equals("interested")
-            || !interestDTOFromDB.getObject().getStatus().equals("interested"))
-          && (!offerDTO.getStatus().equals("not_collected")
-            || !interestDTOFromDB.getObject().getStatus().equals("not_collected"))) {
+      if (!offerDTO.getStatus().equals("interested")
+          && !offerDTO.getStatus().equals("not_collected")) {
         throw new ForbiddenException("L'offre n'est pas en mesure d'être assigné");
       }
 
