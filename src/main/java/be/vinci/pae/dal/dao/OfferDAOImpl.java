54be90414ff64ca1eee93d267735eae46981e882
François Bardijn <francois.bardijn@hotmail.com>
2022-05-06 22:55:56 +0200

    [FIX] PMD error

--- src/main/java/be/vinci/pae/dal/dao/OfferDAOImpl.java
+++ src/main/java/be/vinci/pae/dal/dao/OfferDAOImpl.java
@@ -14,7 +14,6 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
