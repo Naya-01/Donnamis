376e0d79ef4a155e06940cdc2f43188898ce6dee
Stefan <stefan.mircovici@student.vinci.be>
2022-05-07 14:35:47 +0200

    [ADD] test assignOffer prevented member status

--- src/test/java/be/vinci/pae/business/ucc/InterestUCCImplTest.java
+++ src/test/java/be/vinci/pae/business/ucc/InterestUCCImplTest.java
@@ -1128,6 +1128,52 @@ class InterestUCCImplTest {
     );
   }
 
+  @DisplayName("Test assignOffer with prevented member status")
+  @Test
+  public void testAssignOfferWithPreventedMemberStatus() {
+    ObjectDTO newObject = objectFactory.getObjectDTO();
+    newObject.setIdObject(objectDTO.getIdObject());
+    newObject.setVersion(26);
+
+    objectDTO.setVersion(26);
+
+    OfferDTO offerDTOFromGetLastOne = offerFactory.getOfferDTO();
+    offerDTOFromGetLastOne.setIdOffer(18);
+    offerDTOFromGetLastOne.setVersion(14);
+    offerDTOFromGetLastOne.setObject(newObject);
+    offerDTOFromGetLastOne.setStatus("not_collected");
+
+    OfferDTO offerDTO = offerFactory.getOfferDTO();
+    offerDTO.setObject(interestDTO.getObject());
+    offerDTO.setIdOffer(18);
+    offerDTO.setVersion(14);
+
+    MemberDTO memberDTO = memberFactory.getMemberDTO();
+    memberDTO.setMemberId(3);
+    memberDTO.setStatus("prevented");
+
+    interestDTO.setIdMember(memberDTO.getMemberId());
+    interestDTO.getObject().setIdOfferor(memberDTO.getMemberId());
+    interestDTO.setOffer(offerDTO);
+    interestDTO.setMember(memberDTO);
+
+    Mockito.when(mockInterestDAO.getOne(interestDTO.getIdObject(), memberDTO.getMemberId()))
+        .thenReturn(interestDTO);
+    Mockito.when(mockObjectDAO.getOne(interestDTO.getIdObject()))
+        .thenReturn(interestDTO.getObject());
+    Mockito.when(mockMemberDAO.getOne(interestDTO.getIdMember()))
+        .thenReturn(memberDTO);
+    Mockito.when(mockOfferDAO.getLastObjectOffer(interestDTO.getIdObject()))
+        .thenReturn(offerDTOFromGetLastOne);
+
+    assertAll(
+        () -> assertThrows(ForbiddenException.class,
+            () -> interestUCC.assignOffer(interestDTO, memberDTO)),
+        () -> Mockito.verify(mockDalService, Mockito.atLeastOnce()).startTransaction(),
+        () -> Mockito.verify(mockDalService, Mockito.atLeastOnce()).rollBackTransaction()
+    );
+  }
+
 
   @DisplayName("Test assignOffer with available offer status")
   @Test
